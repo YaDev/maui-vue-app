b7b2160c4381cef5aafb59a0bc5a565176a3cf8a
Yaser <nightboy201@gmail.com>
2024-08-08 21:50:35 -0500

    fixed dotnet check call

--- BlazorMaui.UI/src/main.ts
+++ BlazorMaui.UI/src/main.ts
@@ -1,23 +1,23 @@
 import { createApp, h } from "vue";
 import App from "./App.vue";
-import "@popperjs/core";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
 /*
     Wait for the webview to load Blazor Framework and start it if needed
 */
-if(window.hasOwnProperty("DotNet")) {
-    try {
-        await new Promise(r => setTimeout(r, 100));
+if (window.hasOwnProperty("DotNet")) {
+    new Promise(r => setTimeout(r, 100)).then(() => {
+      try {
         window.DotNet.invokeMethod("", "");
-    }
-    catch(error: any) {
-        if(error.message.includes("No call dispatcher")){
-            window.Blazor.start();
+      }
+      catch (error: any) {
+        if (error.message.includes("No call dispatcher")) {
+          window.Blazor.start();
         }
-    }
-}
+      }
+    });
+  }
 
 createApp({
     setup: () => {},
